32ce5033f96ed1d693935c6db47dd4b3b22c8cea
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-07-30 20:48:38 -0300

    Change LeftMenuFooter

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * LeftMenuFooter
+ *
+ */
+
+import React from "react";
+import { defineMessages, FormattedMessage } from "react-intl";
+
+import Wrapper from "./Wrapper";
+import messages from "./messages.json";
+
+defineMessages(messages);
+
+function LeftMenuFooter() {
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        <FormattedMessage
+          id={messages.poweredBy.id}
+          defaultMessage={messages.poweredBy.defaultMessage}
+          key="poweredBy"
+        />
+        <a
+          key="website"
+          href="https://reactavancado.com.br"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Avançado
+        </a>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default LeftMenuFooter;
