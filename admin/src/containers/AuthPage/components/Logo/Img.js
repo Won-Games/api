451606bf62b6c76f614e5c241383b447cf434e7e
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-07-30 19:32:21 -0300

    Custom Login logo

---
+++ admin/src/containers/AuthPage/components/Logo/Img.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Img = styled.img`
+  height: 80px;
+`;
+
+export default Img;
---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import LogoStrapi from "../../../../assets/images/logo-won-dark.svg";
+import Img from "./Img";
+
+const Logo = () => <Img src={LogoStrapi} alt="Won Games" />;
+
+export default Logo;
