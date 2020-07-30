eec778ec8bdcebeb2e1925e97d829c5175548eec
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-07-30 20:05:09 -0300

    Refactor LeftMenuHeader styles

--- admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -1,38 +1,21 @@
-import styled from "styled-components";
-import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 
 import Logo from "../../../assets/images/logo-won.svg";
 
 const Wrapper = styled.div`
-  background-color: #007eff;
-  height: ${(props) => props.theme.main.sizes.leftMenu.height};
+  ${({ theme }) => css`
+    background-color: ${theme.main.colors.won.blue};
+    height: ${theme.main.sizes.leftMenu.height};
 
-  .projectName {
-    display: block;
-    height: ${(props) => props.theme.main.sizes.leftMenu.height};
-    background-image: url(${Logo});
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: 12rem;
-  }
+    .projectName {
+      display: block;
+      height: ${theme.main.sizes.leftMenu.height};
+      background-image: url(${Logo});
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: 12rem;
+    }
+  `}
 `;
-
-Wrapper.defaultProps = {
-  theme: {
-    main: {
-      colors: {
-        leftMenu: {},
-      },
-      sizes: {
-        header: {},
-        leftMenu: {},
-      },
-    },
-  },
-};
-
-Wrapper.propTypes = {
-  theme: PropTypes.object,
-};
 
 export default Wrapper;
