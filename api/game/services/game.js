b3e9c9c4aeee913d3e1544ce16c4ada3947c73f1
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-08-03 22:00:37 -0300

    Add method to getGameInfo

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
@@ -7,12 +7,40 @@
 
 const axios = require("axios");
 
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const ratingElement = dom.window.document.querySelector(
+    ".age-restrictions__icon use"
+  );
+
+  const description = dom.window.document.querySelector(".description");
+
+  return {
+    rating: ratingElement
+      ? ratingElement
+          .getAttribute("xlink:href")
+          .replace(/_/g, "")
+          .replace(/[^\w-]+/g, "")
+      : "Free",
+    short_description: description.textContent.trim().slice(0, 160),
+    description: description.innerHTML,
+  };
+}
+
 module.exports = {
   populate: async (params) => {
-    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
 
-    const { data: { products } } = await axios.get(gogApiUrl)
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
 
-    console.log(products[0])
-  }
+    // console.log(products[0]);
+
+    console.log(await getGameInfo(products[1].slug));
+  },
 };
