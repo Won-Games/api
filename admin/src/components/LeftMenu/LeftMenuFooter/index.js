efa2fd7b86e1e0fea152385ffbb009ad5532881c
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-09-16 12:51:52 -0300

    Update to 3.1.5

--- admin/src/components/LeftMenu/LeftMenuFooter/index.js
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -5,22 +5,14 @@
  */
 
 import React from "react";
-import { defineMessages, FormattedMessage } from "react-intl";
 
 import Wrapper from "./Wrapper";
-import messages from "./messages.json";
-
-defineMessages(messages);
 
 function LeftMenuFooter() {
   return (
     <Wrapper>
       <div className="poweredBy">
-        <FormattedMessage
-          id={messages.poweredBy.id}
-          defaultMessage={messages.poweredBy.defaultMessage}
-          key="poweredBy"
-        />
+        <span>Mantido por </span>
         <a
           key="website"
           href="https://reactavancado.com.br"
--- api/game/services/game.js
+++ api/game/services/game.js
@@ -56,7 +56,7 @@ async function create(name, entityName) {
   if (!item) {
     return await strapi.services[entityName].create({
       name,
-      slug: slugify(name, { lower: true }),
+      slug: slugify(name, { strict: true, lower: true }),
     });
   }
 }
