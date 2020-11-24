f13fa2f3b463a90523574396867739a3cc2b00a6
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-11-24 20:19:37 -0300

    Change rating to be always BR0

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -24,19 +24,10 @@ async function getGameInfo(slug) {
     const body = await axios.get(`https://www.gog.com/game/${slug}`);
     const dom = new JSDOM(body.data);
 
-    const ratingElement = dom.window.document.querySelector(
-      ".age-restrictions__icon use"
-    );
-
     const description = dom.window.document.querySelector(".description");
 
     return {
-      rating: ratingElement
-        ? ratingElement
-            .getAttribute("xlink:href")
-            .replace(/_/g, "")
-            .replace(/[^\w-]+/g, "")
-        : "BR0",
+      rating: "BR0",
       short_description: description.textContent.trim().slice(0, 160),
       description: description.innerHTML,
     };
