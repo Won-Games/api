b371472cb10db7e930adb19e1a634f937619e738
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-04-14 20:43:27 -0300

    Search Cart Game info

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -6,7 +6,23 @@
  */
 
 module.exports = {
-  createPaymentIntent: () => {
-    return "Hello World!";
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body;
+
+    let games = [];
+
+    await Promise.all(
+      cart?.map(async (game) => {
+        const validatedGame = await strapi.services.game.findOne({
+          id: game.id,
+        });
+
+        if (validatedGame) {
+          games.push(validatedGame);
+        }
+      })
+    );
+
+    return games;
   },
 };
