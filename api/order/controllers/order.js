5638bcb68cfe9de8fb9336b1072d1aeb28be76f4
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-04-15 13:07:17 -0300

    Create paymentIntent on Stripe

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -37,6 +37,18 @@ module.exports = {
       };
     }
 
-    return { total_in_cents: total * 100, games };
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total * 100,
+        currency: "usd",
+        metadata: { integration_check: "accept_a_payment" },
+      });
+
+      return paymentIntent;
+    } catch (err) {
+      return {
+        error: err.raw.message,
+      };
+    }
   },
 };
