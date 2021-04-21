a5cd74ac30b2e3852de54437151001e1f2d33021
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-04-21 13:32:39 -0300

    Get user info inside create controller

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -51,4 +51,31 @@ module.exports = {
       };
     }
   },
+
+  create: async (ctx) => {
+    // pegar as informações do frontend
+    const { cart, paymentIntentId, paymentMethod } = ctx.request.body;
+
+    // pega o token
+    const token = await strapi.plugins[
+      "users-permissions"
+    ].services.jwt.getToken(ctx);
+
+    // pega o id do usuario
+    const userId = token.id;
+
+    // pegar as informações do usuário
+    const userInfo = await strapi
+      .query("user", "users-permissions")
+      .findOne({ id: userId });
+
+    // pegar os jogos
+    // pegar o total (saber se é free ou não)
+    // pegar o paymentIntentId
+    // pegar as informações do pagamento (paymentMethod)
+    // salvar no banco
+    // enviar um email da compra para o usuário
+
+    return { cart, paymentIntentId, paymentMethod, userInfo };
+  },
 };
