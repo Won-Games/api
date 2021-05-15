4d19a4e442a29038fcdc9b8dc0e2d658e249c86b
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-05-14 22:40:44 -0300

    Add smtp production config

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,19 @@
 module.exports = ({ env }) => ({
+  email: {
+    provider: "nodemailer",
+    providerOptions: {
+      host: env("SMTP_HOST", "smtp-relay.sendinblue.com"),
+      port: env("SMTP_PORT", 587),
+      auth: {
+        user: env("SMTP_USERNAME"),
+        pass: env("SMTP_PASSWORD"),
+      },
+    },
+    settings: {
+      defaultFrom: "wongames@gmail.com",
+      defaultReplyTo: "contact@gmail.com",
+    },
+  },
   upload: {
     provider: "cloudinary",
     providerOptions: {
